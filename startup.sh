b5b697de2e45f993c5af2708aebb0a1aaf22a239
ferryardian1452 <43908594+ferryardian1452@users.noreply.github.com>
2023-03-06 20:29:20 +0700

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+cd ~/$REPL_SLUG
+wp core download
+unzip phpmyadmin.zip
+rm -r phpmyadmin.zip
+cat <<EOF > $HOME/$REPL_SLUG/data/my.cnf
+[server]
+datadir=$HOME/$REPL_SLUG/data
+bind-address = 127.0.0.1
+log-error=_database.err
+innodb-log-file-size=4194304
+lower_case_table_names=1
+log-bin=binlog
+EOF
+
+export MYSQL_HOME=$HOME/$REPL_SLUG/data
+echo $MYSQL_HOME
+mysqld_safe > mariadb.log &
+php -S 0.0.0.0:443 -t .
